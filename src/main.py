0322b8bff2533ef80c27226a8f2e062ab715de82
Noel-Os <osmanaj.noel0@gmail.com>
2022-04-28 17:56:20 +0200

    create PDF from HTML with wkhtmltopdf

--- src/main.py
+++ src/main.py
@@ -1,85 +1,116 @@
-import json
-from fpdf import FPDF, HTMLMixin
+import ftplib
 import stripe
+import pdfkit
+from html.parser import HTMLParser
+from datetime import datetime as d
+
+CustomerId = ""
+city = ""
+zip = ""
+street = ""
+country = ""
+
+def createProduct():
+    stripe.Product.create(name="Nintendo Switch")
 
 def createPDF(data):
-    html = """
-    <H1 align="center">html2fpdf</H1>
-    <h2>Basic usage</h2>
-    <p>You can now easily print text mixing different
-    styles : <B>bold</B>, <I>italic</I>, <U>underlined</U>, or
-    <B><I><U>all at once</U></I></B>!<BR>You can also insert links
-    on text, such as <A HREF="http://www.fpdf.org">www.fpdf.org</A>,
-    or on an image: click on the logo.<br>
-    <center>
-    <A HREF="http://www.fpdf.org"><img src="tutorial/logo.png" width="104" height="71"></A>
-    </center>
-    <h3>Sample List</h3>
-    <ul><li>option 1</li>
-    <ol><li>option 2</li></ol>
-    <li>option 3</li></ul>
 
-    <table border="0" align="center" width="50%">
-    <thead><tr><th width="30%">Header 1</th><th width="70%">header 2</th></tr></thead>
-    <tbody>
-    <tr><td>cell 1</td><td>cell 2</td></tr>
-    <tr><td>cell 2</td><td>cell 3</td></tr>
-    </tbody>
+    html = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+    <h1 align="center">Payment Receipt</h1>
+
+    <table style="width:100%">
+        <tbody>
+            <tr>
+                <th><h2>Invoice To</h2></th>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td>""" + stripe.Customer.retrieve(CustomerId)["name"] + """</td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td>""" + street + "<br>" + city + " " + zip + "<br>" + country + """</td>
+            </tr>
+            <tr>
+                <th>Date</th>
+                <td>""" + d.now().strftime("%H:%M:%S %Y-%m-%d") + """</td>
+            </tr>
+        </tbody>
     </table>
+
+    <hr>
+
+    </body>
+    </html>
+    
     """
 
-    class MyFPDF(FPDF, HTMLMixin):
-        pass
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    pdfkit.from_string(html, r'documentation\Payment.pdf', configuration=config)
 
-    pdf = MyFPDF()
-    pdf.add_page()
-    pdf.set_font('arial', '', 14)
-    pdf.write_html(html)
-    pdf.output("./documentation/Payment.pdf")
-
-
-    # pdf = FPDF()
-    #
-    # pdf.add_page()
-    #
-    # pdf.set_font("Arial", size=40, style="B")
-    #
-    # pdf.cell(200, 10, txt="Payment Receipt",
-    #          ln=1, align='C', )
-    #
-    # pdf.set_font("Arial", size=30, style="B")
-    #
-    # pdf.cell(100, 30, txt="Bestellung an:",
-    #         ln=2, align='L')
-    #
-    # pdf.set_font("Arial", size=15, style="B")
-    #
-    # pdf.cell(10, 10, txt="Name:",
-    #         ln=3, align='L')
-    #
-    # pdf.cell(50, 10, txt="Adresse:",
-    #          ln=4, align='L')
-    #
-    # pdf.cell(50, 10, txt="Datum:",
-    #          ln=4, align='L')
-    #
-    # pdf.cell(50, 10, txt="Bezahlungs Methode:",
-    #          ln=4, align='L')
-    #
-    # pdf.set_font("Arial", size=15)
-    #
-    # pdf.cell(10, 5, txt="Test Test",
-    #          ln=3, align='C')
 
 if __name__ == '__main__':
     stripe.api_key = "sk_test_51KiDANElM3dtUv2KoZ7ttJQNgVlkOYDQnlVnl4etIKaEo0PEZFzIgT8znwHgrmAzj3VSJIDa64Uu1WbobPOcZFAb00Yzh1InKZ"
 
+    #stripe.Customer.update()
+
+    #createProduct()
+    p = stripe.Product.list()
+    #print(p["data"])
+
+    payment = stripe.PaymentIntent.list()
+    #print(payment["data"])
+
+    #stripe.Customer.delete("cus_LaOjbIBdZt0Tlz")
+
+    # stripe.Customer.modify("cus_LaOjbIBdZt0Tlz",
+    #                        address=[
+    #                            {
+    #                                "city": "Bruettisellen",
+    #                                "country": "Switzerland",
+    #                                "line1": "Im Talacher 13",
+    #                                "postal_code": "8306",
+    #                            },
+    #                        ],
+    #                        )
+
+    Customer = stripe.Customer.create(
+        balance=100,
+        description="Test Customer",
+        email="osmanaj.noel0@gmail.com",
+        name="Noel Osmanaj",
+        address={
+                "city": "Bruettisellen",
+                "country": "CH",
+                "line1": "Im Talacher 13",
+                "postal_code": "8306",
+        },
+    )
+    CustomerId = Customer["id"]
+
+    print(CustomerId)
+    print(Customer["address"])
+
+    city = str(stripe.Customer.retrieve(CustomerId)["address"]["city"])
+    zip = str(stripe.Customer.retrieve(CustomerId)["address"]["postal_code"])
+    street = str(stripe.Customer.retrieve(CustomerId)["address"]["line1"])
+    country = str(stripe.Customer.retrieve(CustomerId)["address"]["country"])
+
     createPDF(stripe.PaymentIntent.retrieve("pi_3KnIjZElM3dtUv2K1qsvQn2j"))
 
-    #stripe.Customer.create(
-    #    balance=100,
-    #    description="My First Test Customer (created for API docs)",
-    #)
+
+
+    stripe.Customer.delete(Customer["id"])
+
+    #print(stripe.Customer.list(limit=1))
 
     #print(stripe.Customer.list(limit=3))
 
@@ -91,4 +122,4 @@ if __name__ == '__main__':
 
     t = stripe.PaymentIntent.retrieve("pi_3KnIjZElM3dtUv2K1qsvQn2j")
 
-    print(t['amount'])
+    #print(t['amount'])
